b58a8f8728226f6946e3474a578ad4e54bbb1f6a
Aplomb Chen <aplomb.chen@gmail.com>
2016-08-26 20:11:17 +0800

    work from home

--- ScriptCore/src/script/memodb/data/MDataFileGroup.java
+++ ScriptCore/src/script/memodb/data/MDataFileGroup.java
@@ -1,6 +1,10 @@
 package script.memodb.data;
 
 import java.io.File;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -9,12 +13,17 @@ import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.filefilter.FileFilterUtils;
 import org.apache.commons.lang.StringUtils;
 
+import chat.logs.LoggerEx;
+
 /**
  * @author aplombchen
  *
  */
 public class MDataFileGroup<T extends MDataFile<?>> {
+	private static final String TAG = MDataFileGroup.class.getSimpleName();
 	private List<T> files;
+	
+	public static final int AVAILABLE_FILESIZE = 5 * 1024 * 1024;
 
 	public MDataFileGroup(String basePath, String fileName) {
 		files = new ArrayList<T>();
@@ -25,14 +34,51 @@ public class MDataFileGroup<T extends MDataFile<?>> {
 			String theFileName = file.getName();
 			theFileName = theFileName.replace(fileName, "");
 			if(StringUtils.isBlank(theFileName)) {
-				
-				files.add(0, element);
+				T dataFile = createFile(file);
+				if(dataFile != null) {
+					files.add(0, dataFile);
+				}
+			} else {
+				int number = -1;
+				try {
+					number = Integer.parseInt(theFileName);
+				} catch (Exception e) {
+				}
+				if(number > 0) {
+					T dataFile = createFile(file);
+					if(dataFile != null) {
+						files.add(number, dataFile);
+					}
+				}
 			}
-			System.out.println(file);
 		}
 	}
 	
-	private MDataFile<?> createFile(File file) {
+	private T createFile(File file) {
+		Class<? extends MDataFile<?>> fileClass = null;
+		Type[] types = this.getClass().getGenericInterfaces();
+		for (Type type : types) {
+			if (type instanceof ParameterizedType) {
+				ParameterizedType pType = (ParameterizedType) type;
+				if (pType.getRawType().equals(MDataFile.class)) {
+					Type[] params = pType.getActualTypeArguments();
+					if (params != null && params.length == 1) {
+						fileClass = (Class<? extends MDataFile<?>>) params[0];
+					}
+				}
+			}
+		}
+		if(fileClass != null) { 
+			try {
+				Constructor<?> constructor = fileClass.getConstructor(String.class);
+				return (T) constructor.newInstance(file.getAbsolutePath());
+			} catch (Throwable t) {
+				t.printStackTrace();
+				LoggerEx.error(TAG, "Create MDataFile " + file + " failed, " + t.getMessage());
+			}
+		} else {
+			LoggerEx.error(TAG, "Create MDataFile " + file + " failed, because fileClass not found, " + this.getClass());
+		}
 		return null;
 	}
 	
